5824b204da0627691d6dca8ad2b3fce3a891f6cb
Kieran Reynolds <reynolds17.kr@gmail.com>
2021-10-28 16:19:20 +0100

    Add build_coord_files script

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     description='Small RNA scripts developed for use in the Hunt Lab',
     author='Kieran Reynolds',
     packages=['hlsmallrna'],
-    scripts=['bin/label_for_unitas'],
+    scripts=['bin/label_for_unitas', 'bin/build_coord_files'],
     entry_points = {
         'console_scripts': [
             'hlsmallrna = hlsmallrna:climain',                  
-        ],              
+        ]          
     }
 )
